9f1a87116e287b6d16d223b92f1e6c37ae31fba0
alanmartinc <alancabot.ac@gmail.com>
2022-05-01 01:23:12 -0300

    Se agrega home

--- pokemon/src/routes/index.jsx
+++ pokemon/src/routes/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Landing from '../views/Landing';
 import Login from '../views/Login';
+import Home from '../views/Home';
 import Error404 from '../views/CodeError/Error404';
 
 export default function Router() {
@@ -12,6 +13,7 @@ export default function Router() {
 				<Routes>
 					<Route exact path='/' element={<Landing />} />
 					<Route exact path='/login' element={<Login />} />
+					<Route exact path='/home' element={<Home />} />
 					<Route path='*' element={<Error404 />} />
 				</Routes>
 			</Layout>
